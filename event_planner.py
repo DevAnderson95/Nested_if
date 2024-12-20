c6b4ba6bd65721ed4216bb411502cee21752abec
DevAnderson95 <DevAnderson95@gmail.com>
2024-12-20 04:06:31 -0500

    Added mored recommendations

--- event_planner.py
+++ event_planner.py
@@ -1,10 +1,20 @@
 # Task 1 Buggy Code
 
 attendees = int(input("Enter number of attendees: "))
-venue = "large hall" if attendees >= 100 else print("conference room")
+venue = print("large hall") if attendees >= 100 else print("conference room")
 
-print(venue)
+
 
 # Task 2 56
 
-sound = input("Would you like to add a microphone and speaker?: ") if attendees >= 50 else print(input("How about a big screen?: "))  
+if attendees >= 50:
+    sound = input("Would you like to add a microphone and speaker?: ") 
+    response = print("Microphone and stand will be added.") if sound == "yes" else print("Audio will be tuned to your liking.")
+
+# Task 3 
+food = input("Vegan Preference?: ")
+
+if food == "yes":
+    print("Veggie Delight Caterers has great services!")
+else:
+    print("Gourment Meals Caterers has great services!")
